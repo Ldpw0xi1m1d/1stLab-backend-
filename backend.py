0db189a45fdef6d9c7bff74130712dec4742ad2b
Ldpw0xi1m1d <dimas.batrakov.1995@bk.ru>
2024-09-23 16:52:48 +0300

    2nd commit

--- backend.py
+++ backend.py
@@ -2,3 +2,17 @@ def backClick(text):
     with open("info.txt", "a") as f:
         f.write("\n" + text + " ")
         f.close()
+
+def backGetData():
+    with open("info.txt", "r") as f:
+        lines = f.read().split("\n")
+        f.close()
+        text = ""
+        i = 0
+        for line in lines:
+            if i > 0:
+                text += line + ", "
+                i += 1
+            else: text += line
+            i += 1
+        return text
